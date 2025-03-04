b954aa98d33c57f63edc1b0d8d016534d6349a9c
SachinPattar8866 <pattarsachin10@gmail.com>
2025-03-04 18:43:19 +0530

    Initial commit

---
+++ src/main/java/com/example/LoanNest/LoanNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoanNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanNestApplication.class, args);
+	}
+
+}
