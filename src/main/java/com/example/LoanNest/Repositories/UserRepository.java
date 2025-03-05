fec984aa934c070b88e6881114971467945403a7
SachinPattar8866 <pattarsachin10@gmail.com>
2025-03-05 17:12:11 +0530

    add-Repositories

---
+++ src/main/java/com/example/LoanNest/Repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.LoanNest.Repositories;
+
+import com.example.LoanNest.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
